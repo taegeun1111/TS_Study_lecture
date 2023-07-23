c94d7c9b880ddee1bf94a92daf9f2c0340de406a
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-23 17:40:44 +0900

    20230723 #4 배열

--- app.ts
+++ app.ts
@@ -7,7 +7,15 @@
 // };
 const person = {
     name : 'Maximilian',
-    age : 30
+    age : 30,
+    hobbies : ['Sports','Cooking']
 };
 
+let favoriteActivities : string[];
+favoriteActivities= ['Sports'];
+
 console.log(person.name)
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
