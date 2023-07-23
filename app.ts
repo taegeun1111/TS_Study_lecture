dee0abd1b5613d76d6d428af702bd244dbf495bc
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-23 16:20:24 +0900

    20230723 #2 타입 함수 사용 예제

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-function add(n1,n2){
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('형식이 다릅니다.')
+    // }
+    const result = n1+n2;
+    if (showResult === true) {
+        // console.log(phrase + (n1 + n2));
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
 }
 
-const number1 = 5;
+const number1 = 5.0;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1,number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
