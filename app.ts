459f0bff057432b8a887ce89be7de4d89b9badae
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-23 17:49:01 +0900

    20230723 #5 tuple

--- app.ts
+++ app.ts
@@ -5,16 +5,27 @@
 //     name : 'Maximilian',
 //     age : 30
 // };
-const person = {
+const person:{
+    name : string;
+    age : number;
+    hobbies : string[];
+    role : [number,string];
+} = {
     name : 'Maximilian',
     age : 30,
-    hobbies : ['Sports','Cooking']
+    hobbies : ['Sports','Cooking'],
+    role : [2, 'author']
 };
+
+person.role.push('admin');
+// person.role[1] = 10;
+
+person.role = [0,'qwe'];
 
 let favoriteActivities : string[];
 favoriteActivities= ['Sports'];
 
-console.log(person.name)
+console.log(person)
 
 for (const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
