b34046ba884c6295bb52a1c3ce0434c2309d562a
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-24 14:05:46 +0900

    20230724 #1 union Type

--- app.ts
+++ app.ts
@@ -1,32 +1,15 @@
-// const person: {
-//     name : string,
-//     age : number
-// } = {
-//     name : 'Maximilian',
-//     age : 30
-// };
-const person:{
-    name : string;
-    age : number;
-    hobbies : string[];
-    role : [number,string];
-} = {
-    name : 'Maximilian',
-    age : 30,
-    hobbies : ['Sports','Cooking'],
-    role : [2, 'author']
-};
-
-person.role.push('admin');
-// person.role[1] = 10;
-
-person.role = [0,'qwe'];
-
-let favoriteActivities : string[];
-favoriteActivities= ['Sports'];
-
-console.log(person)
-
-for (const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
+function combine(input1:number | string, input2:number | string){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number'){
+    result = input1+input2;
+    } else{
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+
+const combinedAges = combine(30,26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max','Anna');
+console.log(combinedNames);
---
+++ objs-array-enums.ts
@@ -0,0 +1,49 @@
+// const person: {
+//     name : string,
+//     age : number
+// } = {
+//     name : 'Maximilian',
+//     age : 30
+// };
+// const person:{
+//     name : string;
+//     age : number;
+//     hobbies : string[];
+//     role : [number,string];
+// } = {
+//     name : 'Maximilian',
+//     age : 30,
+//     hobbies : ['Sports','Cooking'],
+//     role : [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
+    name : 'Maximilian',
+    age : 30,
+    hobbies : ['Sports','Cooking'],
+    role : Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0,'qwe'];
+
+let favoriteActivities : any[];
+favoriteActivities= ['Sports'];
+
+console.log(person)
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN){
+    console.log('is AUTHOR');
+}
