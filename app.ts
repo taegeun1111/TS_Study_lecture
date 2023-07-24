bb7a3f5784c49e5d0037115d8b79134a9c956fa1
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-24 15:54:52 +0900

    20230724 #6 never Type

--- app.ts
+++ app.ts
@@ -1,28 +1,15 @@
-function add(n1: number, n2: number): number {
-    return (n1 + n2);
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
 
-function printResult(num: number) {
-    return console.log(`Result : ${num}`)
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(5, 12));
-
-// let combineValues : Function;
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-// combineValues = printResult;
-console.log(combineValues(7, 7));
-
-// combineValues = 5;
-
-addAndHandle(10,20, (result)=>{
-    console.log(result);
-    return result;
-});
-
+generateError('An error occurred!', 500);
---
+++ functions.ts
@@ -0,0 +1,28 @@
+function add(n1: number, n2: number): number {
+    return (n1 + n2);
+}
+
+function printResult(num: number) {
+    return console.log(`Result : ${num}`)
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+printResult(add(5, 12));
+
+// let combineValues : Function;
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(7, 7));
+
+// combineValues = 5;
+
+addAndHandle(10,20, (result)=>{
+    console.log(result);
+    return result;
+});
+
