ebd13ea3a95ea1f0e4ac571dcd1339d13f071b37
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-24 14:19:29 +0900

    20230724 #3 TypeAlias ✨

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
 function combine(
-    input1:number | string,
-    input2:number | string,
-    resultConversation : 'as-number' | 'as-text'
-    ){
+    input1: Combinable,
+    input2: Combinable,
+    resultConversation: ConversionDescriptor
+) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversation === 'as-number'){
-    result = +input1+ +input2;
-    } else{
+    if (typeof input1 === 'number' &&
+        typeof input2 === 'number' ||
+        resultConversation === 'as-number'
+    ) {
+        result = +input1 + +input2;
+    } else {
         result = input1.toString() + input2.toString();
     }
     // if (resultConversation === 'as-number'){
@@ -18,11 +24,11 @@ function combine(
     return result;
 }
 
-const combinedAges = combine(30,26, 'as-number');
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedStringAges = combine('30','26', 'as-number');
+const combinedStringAges = combine('30', '26', 'as-number');
 console.log(combinedStringAges);
 
-const combinedNames = combine('Max','Anna', 'as-text');
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
