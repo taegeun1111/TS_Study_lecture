66c7f187190925ef4160b3376836d3d476cadc0d
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-24 14:15:37 +0900

    20230724 #2 literal Type

--- app.ts
+++ app.ts
@@ -1,15 +1,28 @@
-function combine(input1:number | string, input2:number | string){
+function combine(
+    input1:number | string,
+    input2:number | string,
+    resultConversation : 'as-number' | 'as-text'
+    ){
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-    result = input1+input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversation === 'as-number'){
+    result = +input1+ +input2;
     } else{
         result = input1.toString() + input2.toString();
     }
+    // if (resultConversation === 'as-number'){
+    //     return +result;
+    // }else{
+    //     return result.toString();
+    // }
+
     return result;
 }
 
-const combinedAges = combine(30,26);
+const combinedAges = combine(30,26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Max','Anna');
+const combinedStringAges = combine('30','26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max','Anna', 'as-text');
 console.log(combinedNames);
