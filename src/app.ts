77ded1ee29e951de40ef395f214c038d5170b65c
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-07-25 20:55:05 +0900

    20230725 #8 Optional properties

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,47 @@
-//객체의 구조를 정의할 수 있게 해준다.
-interface Greetable {
-    name: string;
+// type AddFn = (a:number, b:number) => number;
+interface AddFn {
+    (a:number,b:number) :number;
+}
 
+
+let add : AddFn;
+
+add = (n1 : number, n2:number) =>{
+    return n1 + n2;
+};
+
+interface Named{
+    readonly name ?:string;
+    outputName ? : string;
+}
+
+//interface : 객체의 구조를 정의할 수 있게 해준다.
+interface Greetable extends Named{
     greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    name: string;
+    name?: string;
     age = 30;
 
-    constructor(n: string) {
+    constructor(n ?: string) {
+        if (n){
         this.name = n;
+        }
     }
 
     greet(phrase: string): void {
+        if (this.name){
+            console.log(phrase+ ' ' + this.name);
+        }else{
+            console.log('Hi!')
+        }
     }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person();
 
 user1.greet("하이");
 console.log(user1);
